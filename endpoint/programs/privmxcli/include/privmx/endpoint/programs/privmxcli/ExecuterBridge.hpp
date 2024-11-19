03a06c8a1d670dcbc1ab89c9a794265aab1b0513
Jerzy Czarkowski <jczarkowski@privmx.com>
2024-11-19 13:02:37 +0100

    test: bridge descriptions (part 2)

--- endpoint/programs/privmxcli/include/privmx/endpoint/programs/privmxcli/ExecuterBridge.hpp
+++ endpoint/programs/privmxcli/include/privmx/endpoint/programs/privmxcli/ExecuterBridge.hpp
@@ -374,33 +374,184 @@ private:
     };
 
     const std::unordered_map<func_enum, std::string> functions_bridge_help_description = {
-        {bridge_setBridgeApiCreds, ""},
-        {bridge_manager_auth, ""},
-        {bridge_manager_createApiKey, ""},
-        {bridge_manager_getApiKey, ""},
-        {bridge_manager_listApiKeys, ""},
-        {bridge_manager_updateApiKey, ""},
-        {bridge_manager_deleteApiKey, ""},
-        {bridge_solution_getSolution, ""},
-        {bridge_solution_listSolutions, ""},
-        {bridge_solution_createSolution, ""},
-        {bridge_solution_updateSolution, ""},
-        {bridge_solution_deleteSolution, ""},
-        {bridge_context_getContext, ""},
-        {bridge_context_listContexts, ""},
-        {bridge_context_listContextsOfSolution, ""},
-        {bridge_context_createContext, ""},
-        {bridge_context_updateContext, ""},
-        {bridge_context_deleteContext, ""},
-        {bridge_context_addSolutionToContext, ""},
-        {bridge_context_removeSolutionFromContext, ""},
-        {bridge_context_addUserToContext, ""},
-        {bridge_context_removeUserFromContext, ""},
-        {bridge_context_removeUserFromContextByPubKey, ""},
-        {bridge_context_getUserFromContext, ""},
-        {bridge_context_getUserFromContextByPubKey, ""},
-        {bridge_context_listUsersFromContext, ""},
-        {bridge_context_setUserAcl, ""},
+        {bridge_setBridgeApiCreds,
+            "bridge.setBridgeApiCreds APIKEY_ID, APIKEY_SECRET, URL\n"
+        },
+        {bridge_manager_auth,
+            "bridge.manager.auth JSON_OBJECT\n"
+            "\tjson format - {grantType: \"api_key_credentials\", apiKeyId: string, apiKeySecret: string, scope?: string[]}"
+            "\t\tapiKeyId [STRING] - Api key id (length in [1,128])"
+            "\t\tapiKeySecret [STRING] - Api key secret (length in [1,128])"
+            "\t\tscope [ARRAY[STRING]] - (optional) Requested token scope (length in [0,128])"
+            "\tjson format - {grantType: \"refresh_token\", refreshToken: string}"
+            "\t\trefreshToken [STRING] - Refresh token from earlier invocation (length in [1,2048]))"
+            "\tjson format - {grantType: \"api_key_signature\", apiKeyId: string, signature: string, timestamp: number, nonce: string, scope?: string[], data?: string}"
+            "\t\tapiKeyId [STRING] - Api key id (length in [1,128])"
+            "\t\tsignature [STRING] - EdDSA signature or Hash"
+            "\t\ttimestamp [NUMBER] - Request timestamp (in range: [-∞,∞])"
+            "\t\tnonce [STRING] - Random value used to generate signature (length in [32,64])"
+            "\t\tscope [ARRAY[STRING]] - (optional) Requested token scope (length in [0,128])"
+            "\t\tdata [STRING] - (optional) Optional signed data (length in [0,1024])"
+
+            
+        },
+        {bridge_manager_createApiKey, 
+            "bridge.manager.createApiKey JSON_OBJECT\n"
+            "\tjson format - {name: string, scope: string[], publicKey?: string}\n"
+            "\t\tname [STRING] - New api key name (length in [0,128])"
+            "\t\tscope [ARRAY[STRING]] - New api key scope (length in [0,128])"
+            "\t\tpublicKey [STRING] - (optional) ED25519 PEM encoded public key"
+        },
+        {bridge_manager_getApiKey, 
+            "bridge.manager.getApiKey JSON_OBJECT\n"
+            "\tjson format - {id: string}\n"
+            "\t\tid [STRING] - Api key id (length in [1,128])"
+        },
+        {bridge_manager_listApiKeys,
+            "bridge.manager.listApiKeys JSON_OBJECT\n"
+            "\tjson format - {}\n"
+        },
+        {bridge_manager_updateApiKey, 
+            "bridge.manager.updateApiKey JSON_OBJECT\n"
+            "\tjson format - {id: string, name?: string, scope?: string[], enabled?: boolean}\n"
+            "\t\tid [STRING] - Api key id (length in [1,128])"
+            "\t\tname [STRING] - (optional) Api key name (length in [0,128])"
+            "\t\tscope [ARRAY[STRING]] - (optional) Api key scope (length in [0,128])"
+            "\t\tenabled [BOOL] - (optional) Api key status"
+        },
+        {bridge_manager_deleteApiKey, 
+            "bridge.manager.deleteApiKey JSON_OBJECT\n"
+            "\tjson format - {id: string}\n"
+            "\t\tid [STRING] - Api key id (length in [1,128])"
+        },
+        {bridge_solution_getSolution, 
+            "bridge.solution.getSolution JSON_OBJECT\n"
+            "\tjson format - {id: string}\n"
+            "\t\tid [STRING] - solution's id (length in [1,128])"
+        },
+        {bridge_solution_listSolutions,
+            "bridge.solution.listSolutions JSON_OBJECT\n"
+            "\tjson format - {}\n"
+        },
+        {bridge_solution_createSolution, 
+            "bridge.solution.createSolution JSON_OBJECT\n"
+            "\tjson format - {name: string}\n"
+            "\t\tname [STRING] - solution's name (length in [0,256])"
+        },
+        {bridge_solution_updateSolution, 
+            "bridge.solution.updateSolution JSON_OBJECT\n"
+            "\tjson format - {id: string, name: string}\n"
+            "\t\tid [STRING] - solution's id (length in [1,128])"
+            "\t\tname [STRING] - solution's name (length in [0,256])"
+        },
+        {bridge_solution_deleteSolution, 
+            "bridge.solution.deleteSolution JSON_OBJECT\n"
+            "\tjson format - {id: string}\n"
+            "\t\tid [STRING] - solution's id (length in [1,128])"
+        },
+        {bridge_context_getContext, 
+            "bridge.context.getContext JSON_OBJECT\n"
+            "\tjson format - {contextId: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+        },
+        {bridge_context_listContexts, 
+            "bridge.context.listContexts JSON_OBJECT\n"
+            "\tjson format - {skip: number, limit: number, sortOrder: desc|asc, lastId?: string}\n"
+            "\t\tskip [NUMBER] - (in range: [0,∞])"
+            "\t\tlimit [NUMBER] - (in range: [1,100])"
+            "\t\tsortOrder [STRING] - desc|asc"
+            "\t\tlastId [STRING] - (optional) (length in [1,60])"
+        },
+        {bridge_context_listContextsOfSolution, 
+            "bridge.context.listContextsOfSolution JSON_OBJECT\n"
+            "\tjson format - {solutionId: string, skip: number, limit: number, sortOrder: desc|asc, lastId?: string}\n"
+            "\t\tsolutionId [STRING] - solution's id (length in [1,128])"
+            "\t\tskip [NUMBER] - (in range: [0,∞])"
+            "\t\tlimit [NUMBER] - (in range: [1,100])"
+            "\t\tsortOrder [STRING] - desc|asc"
+            "\t\tlastId [STRING] - (optional) (length in [1,60])"
+        },
+        {bridge_context_createContext, 
+            "bridge.context.createContext JSON_OBJECT\n"
+            "\tjson format - {solution: string, name?: string, description?: string, scope?: private|public, policy?: object}\n"
+            "\t\tsolution [STRING] - solution's id (length in [1,128])"
+            "\t\tname [STRING] - (optional) context's name (length in [0,128])"
+            "\t\tdescription [STRING] - (optional) context's description (length in [0,128])"
+            "\t\tscope [STRING] - (optional) context's scope, public or private"
+            "\t\tpolicy [OBJECT] - (optional) context's policy"
+        },
+        {bridge_context_updateContext, 
+            "bridge.context.updateContext JSON_OBJECT\n"
+            "\tjson format - {contextId: string, name?: string, description?: string, scope?: private|public, policy?: object}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tname [STRING] - (optional) context's name (length in [0,128])"
+            "\t\tdescription [STRING] - (optional) context's description (length in [0,128])"
+            "\t\tscope [STRING] - (optional) context's scope, public or private"
+            "\t\tpolicy [OBJECT] - (optional) context's policy"
+        },
+        {bridge_context_deleteContext, 
+            "bridge.context.deleteContext JSON_OBJECT\n"
+            "\tjson format - {contextId: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+        },
+        {bridge_context_addSolutionToContext, 
+            "bridge.context.addSolutionToContext JSON_OBJECT\n"
+            "\tjson format - {contextId: string, solutionId: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tsolutionId [STRING] - solution's id (length in [1,128])"
+        },
+        {bridge_context_removeSolutionFromContext, 
+            "bridge.context.removeSolutionFromContext JSON_OBJECT\n"
+            "\tjson format - {contextId: string, solutionId: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tsolutionId [STRING] - solution's id (length in [1,128])"
+        },
+        {bridge_context_addUserToContext, 
+            "bridge.context.getUserFromContextByPubKey JSON_OBJECT\n"
+            "\tjson format - {contextId: string, userId: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tuserId [STRING] - user's id (length in [1,128])"
+        },
+        {bridge_context_removeUserFromContext, 
+            "bridge.context.removeUserFromContext JSON_OBJECT\n"
+            "\tjson format - {contextId: string, userId: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tuserId [STRING] - user's id (length in [1,128])"
+        },
+        {bridge_context_removeUserFromContextByPubKey,
+            "bridge.context.removeUserFromContextByPubKey JSON_OBJECT\n"
+            "\tjson format - {contextId: string, userPubKey: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tuserPubKey [STRING] - context user's public key"
+        },
+        {bridge_context_getUserFromContext, 
+            "bridge.context.getUserFromContext JSON_OBJECT\n"
+            "\tjson format - {contextId: string, userId: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tuserId [STRING] - user's id (length in [1,128])"
+        },
+        {bridge_context_getUserFromContextByPubKey, 
+            "bridge.context.getUserFromContextByPubKey JSON_OBJECT\n"
+            "\tjson format - {contextId: string, pubKey: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tpubKey [STRING] - context user's public key"
+        },
+        {bridge_context_listUsersFromContext, 
+            "bridge.context.listUsersFromContext JSON_OBJECT\n"
+            "\tjson format - {contextId: string, skip: number, limit: number, sortOrder: asc|desc, lastId?: string}\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tskip [NUMBER] - (in range: [0,∞])"
+            "\t\tlimit [NUMBER] - (in range: [1,100])"
+            "\t\tsortOrder [STRING] - desc|asc"
+            "\t\tlastId [STRING] - (optional) (length in [1,60])"
+        },
+        {bridge_context_setUserAcl,
+            "bridge.context.setUserAcl JSON_OBJECT\n"
+            "\tjson format - {contextId: string, userId: string, acl: string}]\n"
+            "\t\tcontextId [STRING] - context's id (length in [1,128])"
+            "\t\tuserId [STRING] - user's id (length in [1,128])"
+            "\t\tacl [STRING] - user acl (length in [0,4096])"
+        },
     };
 
     const std::unordered_map<func_enum, std::string> functions_bridge_help_short_description = {
@@ -434,33 +585,33 @@ private:
     };
 
     const std::unordered_map<func_enum, std::string> functions_bridge_action_description = {
-        {bridge_setBridgeApiCreds, ""},
-        {bridge_manager_auth, ""},
-        {bridge_manager_createApiKey, ""},
-        {bridge_manager_getApiKey, ""},
-        {bridge_manager_listApiKeys, "getting list of ApiKeys"},
-        {bridge_manager_updateApiKey, ""},
-        {bridge_manager_deleteApiKey, ""},
-        {bridge_solution_getSolution, ""},
-        {bridge_solution_listSolutions, ""},
-        {bridge_solution_createSolution, ""},
-        {bridge_solution_updateSolution, ""},
-        {bridge_solution_deleteSolution, ""},
-        {bridge_context_getContext, ""},
-        {bridge_context_listContexts, ""},
-        {bridge_context_listContextsOfSolution, ""},
-        {bridge_context_createContext, ""},
-        {bridge_context_updateContext, ""},
-        {bridge_context_deleteContext, ""},
-        {bridge_context_addSolutionToContext, ""},
-        {bridge_context_removeSolutionFromContext, ""},
-        {bridge_context_addUserToContext, ""},
-        {bridge_context_removeUserFromContext, ""},
-        {bridge_context_removeUserFromContextByPubKey, ""},
-        {bridge_context_getUserFromContext, ""},
-        {bridge_context_getUserFromContextByPubKey, ""},
-        {bridge_context_listUsersFromContext, ""},
-        {bridge_context_setUserAcl, ""},
+        {bridge_setBridgeApiCreds, "Setting bridge api credential"},
+        {bridge_manager_auth, "Authenticating"},
+        {bridge_manager_createApiKey, "Creating ApiKey"},
+        {bridge_manager_getApiKey, "Geting ApiKey"},
+        {bridge_manager_listApiKeys, "Geting list of api keys"},
+        {bridge_manager_updateApiKey, "Updating ApiKey"},
+        {bridge_manager_deleteApiKey, "Deleteing ApiKey"},
+        {bridge_solution_getSolution, "Geting solution"},
+        {bridge_solution_listSolutions, "Geting list of solutions"},
+        {bridge_solution_createSolution, "Creating solution"},
+        {bridge_solution_updateSolution, "Updating solution"},
+        {bridge_solution_deleteSolution, "Deleteing solution"},
+        {bridge_context_getContext, "Geting context"},
+        {bridge_context_listContexts, "Geting list of contexts"},
+        {bridge_context_listContextsOfSolution, "Geting list of contexts"},
+        {bridge_context_createContext, "Creating context"},
+        {bridge_context_updateContext, "Updating context"},
+        {bridge_context_deleteContext, "Deleteing context"},
+        {bridge_context_addSolutionToContext, "Adding solution to context"},
+        {bridge_context_removeSolutionFromContext, "Removing solution from context"},
+        {bridge_context_addUserToContext, "Adding user to context"},
+        {bridge_context_removeUserFromContext, "Removing user from context"},
+        {bridge_context_removeUserFromContextByPubKey, "Removing user from context"},
+        {bridge_context_getUserFromContext, "Geting user"},
+        {bridge_context_getUserFromContextByPubKey, "Geting user"},
+        {bridge_context_listUsersFromContext, "Geting list of users"},
+        {bridge_context_setUserAcl, "Updating user ACL"},
     };
 };
 
