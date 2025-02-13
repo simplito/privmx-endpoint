9bf6364a85fc989f8798c55c6ca0ded89defa635
Patryk Kisielewski <pkisielewski@simplito.com>
2025-02-13 12:32:15 +0100

    feat(stream): add low api interface and move WebRTCInterface to public headers

---
+++ endpoint/stream/stream/include_pub/privmx/endpoint/stream/StreamApiLow.hpp
@@ -0,0 +1,89 @@
+/*
+PrivMX Endpoint.
+Copyright © 2024 Simplito sp. z o.o.
+
+This file is part of the PrivMX Platform (https://privmx.dev).
+This software is Licensed under the PrivMX Free License.
+
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef _PRIVMXLIB_ENDPOINT_STREAM_STREAMAPILOW_HPP_
+#define _PRIVMXLIB_ENDPOINT_STREAM_STREAMAPILOW_HPP_
+
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+#include <functional>
+
+#include <privmx/endpoint/core/Connection.hpp>
+#include <privmx/endpoint/core/Types.hpp>
+#include "privmx/endpoint/stream/Types.hpp"
+#include "privmx/endpoint/stream/WebRTCInterface.hpp"
+
+namespace privmx {
+namespace endpoint {
+namespace stream {
+
+class StreamApiLowImpl;
+
+class StreamApiLow {
+public:
+
+    static StreamApiLow create(core::Connection& connetion);
+    StreamApiLow() = default;
+
+    std::string createStreamRoom(
+        const std::string& contextId, 
+        const std::vector<core::UserWithPubKey>& users, 
+        const std::vector<core::UserWithPubKey>&managers,
+        const core::Buffer& publicMeta, 
+        const core::Buffer& privateMeta,
+        const std::optional<core::ContainerPolicy>& policies
+    );
+
+    void updateStreamRoom(
+        const std::string& streamRoomId, 
+        const std::vector<core::UserWithPubKey>& users, 
+        const std::vector<core::UserWithPubKey>&managers,
+        const core::Buffer& publicMeta, 
+        const core::Buffer& privateMeta, 
+        const int64_t version, 
+        const bool force, 
+        const bool forceGenerateNewKey, 
+        const std::optional<core::ContainerPolicy>& policies
+    );
+
+    core::PagingList<StreamRoom> listStreamRooms(const std::string& contextId, const core::PagingQuery& query);
+
+    StreamRoom getStreamRoom(const std::string& streamRoomId);
+
+    void deleteStreamRoom(const std::string& streamRoomId);
+    // Stream
+    int64_t createStream(const std::string& streamRoomId, int64_t localStreamId, std::shared_ptr<WebRTCInterface> webRtc);
+    
+    void publishStream(int64_t localStreamId);
+
+    int64_t joinStream(const std::string& streamRoomId, const std::vector<int64_t>& streamsId, const streamJoinSettings& settings, int64_t localStreamId, std::shared_ptr<WebRTCInterface> webRtc);
+
+    std::vector<Stream> listStreams(const std::string& streamRoomId);
+
+    void unpublishStream(int64_t localStreamId);
+
+    void leaveStream(int64_t localStreamId);
+
+    std::shared_ptr<StreamApiLowImpl> getImpl() const { return _impl; }
+
+private:
+    void validateEndpoint();
+    StreamApiLow(const std::shared_ptr<StreamApiLowImpl>& impl);
+    std::shared_ptr<StreamApiLowImpl> _impl;
+};
+
+}  // namespace stream
+}  // namespace endpoint
+}  // namespace privmx
+
+#endif  // _PRIVMXLIB_ENDPOINT_STREAM_STREAMAPILOW_HPP_
---
+++ endpoint/stream/stream/src/StreamApiLow.cpp
@@ -0,0 +1,191 @@
+/*
+PrivMX Endpoint.
+Copyright © 2024 Simplito sp. z o.o.
+
+This file is part of the PrivMX Platform (https://privmx.dev).
+This software is Licensed under the PrivMX Free License.
+
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <privmx/endpoint/core/Exception.hpp>
+#include <privmx/endpoint/core/JsonSerializer.hpp>
+#include <privmx/endpoint/core/ExceptionConverter.hpp>
+#include <privmx/endpoint/core/Connection.hpp>
+#include <privmx/endpoint/core/ConnectionImpl.hpp>
+#include <privmx/endpoint/core/EventVarSerializer.hpp>
+#include <privmx/endpoint/core/Validator.hpp>
+
+#include "privmx/endpoint/stream/StreamApiLow.hpp"
+#include "privmx/endpoint/stream/StreamException.hpp"
+#include "privmx/endpoint/stream/StreamApiLowImpl.hpp"
+
+
+using namespace privmx::endpoint;
+using namespace privmx::endpoint::stream;
+
+StreamApiLow StreamApiLow::create(core::Connection& connection) {
+    try {
+        std::shared_ptr<core::ConnectionImpl> connectionImpl = connection.getImpl();
+        std::shared_ptr<StreamApiLowImpl> impl(new StreamApiLowImpl(
+            connectionImpl->getGateway(),
+            connectionImpl->getUserPrivKey(),
+            connectionImpl->getKeyProvider(),
+            connectionImpl->getHost(),
+            connectionImpl->getEventMiddleware(),
+            connectionImpl->getEventChannelManager(),
+            connectionImpl->getInternalContextEventManager()
+        ));
+        return StreamApiLow(impl);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+StreamApiLow::StreamApiLow(const std::shared_ptr<StreamApiLowImpl>& impl) : _impl(impl) {}
+
+std::string StreamApiLow::createStreamRoom(
+    const std::string& contextId, 
+    const std::vector<core::UserWithPubKey>& users, 
+    const std::vector<core::UserWithPubKey>&managers,
+    const core::Buffer& publicMeta, 
+    const core::Buffer& privateMeta,
+    const std::optional<core::ContainerPolicy>& policies
+) {
+    validateEndpoint();
+    core::Validator::validateId(contextId, "field:contextId ");
+    core::Validator::validateClass<std::vector<core::UserWithPubKey>>(users, "field:users ");
+    core::Validator::validateClass<std::vector<core::UserWithPubKey>>(managers, "field:managers ");
+    try {
+        return _impl->createStreamRoom(contextId, users, managers, publicMeta, privateMeta, policies);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+void StreamApiLow::updateStreamRoom(
+    const std::string& streamRoomId, 
+    const std::vector<core::UserWithPubKey>& users, 
+    const std::vector<core::UserWithPubKey>&managers,
+    const core::Buffer& publicMeta, 
+    const core::Buffer& privateMeta, 
+    const int64_t version, 
+    const bool force, 
+    const bool forceGenerateNewKey, 
+    const std::optional<core::ContainerPolicy>& policies
+) {
+    validateEndpoint();
+    core::Validator::validateId(streamRoomId, "field:streamRoomId ");
+    core::Validator::validateClass<std::vector<core::UserWithPubKey>>(users, "field:users ");
+    core::Validator::validateClass<std::vector<core::UserWithPubKey>>(managers, "field:managers ");
+    try {
+        return _impl->updateStreamRoom(streamRoomId, users, managers, publicMeta, privateMeta, version, force, forceGenerateNewKey, policies);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+core::PagingList<StreamRoom> StreamApiLow::listStreamRooms(const std::string& contextId, const core::PagingQuery& query) {
+    validateEndpoint();
+    core::Validator::validateId(contextId, "field:contextId ");
+    core::Validator::validateClass<core::PagingQuery>(query, "field:query ");
+    try {
+        return _impl->listStreamRooms(contextId, query);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}   
+
+StreamRoom StreamApiLow::getStreamRoom(const std::string& streamRoomId) {
+    validateEndpoint();
+    core::Validator::validateId(streamRoomId, "field:streamRoomId ");
+    try {
+        return _impl->getStreamRoom(streamRoomId);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+void StreamApiLow::deleteStreamRoom(const std::string& streamRoomId) {
+    validateEndpoint();
+    core::Validator::validateId(streamRoomId, "field:streamRoomId ");
+    try {
+        return _impl->deleteStreamRoom(streamRoomId);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+int64_t StreamApiLow::createStream(const std::string& streamRoomId, int64_t localStreamId, std::shared_ptr<WebRTCInterface> webRtc) {
+    validateEndpoint();
+    core::Validator::validateId(streamRoomId, "field:streamRoomId ");
+    try {
+        return _impl->createStream(streamRoomId, localStreamId, webRtc);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+void StreamApiLow::publishStream(int64_t localStreamId) {
+    validateEndpoint();
+    try {
+        return _impl->publishStream(localStreamId);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+int64_t StreamApiLow::joinStream(const std::string& streamRoomId, const std::vector<int64_t>& streamsId, const streamJoinSettings& settings, int64_t localStreamId, std::shared_ptr<WebRTCInterface> webRtc) {
+    validateEndpoint();
+    core::Validator::validateId(streamRoomId, "field:streamRoomId ");
+    try {
+        return _impl->joinStream(streamRoomId, streamsId, settings, localStreamId, webRtc);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+std::vector<Stream> StreamApiLow::listStreams(const std::string& streamRoomId) {
+    validateEndpoint();
+    try {
+        return _impl->listStreams(streamRoomId);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+void StreamApiLow::unpublishStream(int64_t streamId) {
+    validateEndpoint();
+    try {
+        return _impl->unpublishStream(streamId);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+void StreamApiLow::leaveStream(int64_t streamId) {
+    validateEndpoint();
+    try {
+        return _impl->leaveStream(streamId);
+    } catch (const privmx::utils::PrivmxException& e) {
+        core::ExceptionConverter::rethrowAsCoreException(e);
+        throw core::Exception("ExceptionConverter rethrow error");
+    }
+}
+
+
+void StreamApiLow::validateEndpoint() {
+    if(!_impl) throw NotInitializedException();
+}
