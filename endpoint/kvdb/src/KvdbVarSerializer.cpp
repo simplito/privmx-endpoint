dfdff7470fbc02c36ab2f149cefb199c792abe5c
Jerzy Czarkowski <jczarkowski@simplito.com>
2025-05-08 10:31:44 +0200

    fix: VarSerializer KvdbEntry add missing field version

--- endpoint/kvdb/src/KvdbVarSerializer.cpp
+++ endpoint/kvdb/src/KvdbVarSerializer.cpp
@@ -75,7 +75,7 @@ Poco::Dynamic::Var VarSerializer::serialize<kvdb::KvdbEntry>(const kvdb::KvdbEnt
     obj->set("publicMeta", serialize(val.publicMeta));
     obj->set("privateMeta", serialize(val.privateMeta));
     obj->set("data", serialize(val.data));
-    obj->set("authorPubKey", serialize(val.authorPubKey));
+    obj->set("version", serialize(val.version));
     obj->set("statusCode", serialize(val.statusCode));
     obj->set("schemaVersion", serialize(val.schemaVersion));
     return obj;
