0946cab3c6f158cd28273af2fe0b083bf245a290
Patryk Kisielewski <pkisielewski@simplito.com>
2025-05-30 17:28:59 +0200

    feat: Add server slice provider

---
+++ endpoint/store/include/privmx/endpoint/store/File.hpp
@@ -0,0 +1,50 @@
+/*
+PrivMX Endpoint.
+Copyright © 2024 Simplito sp. z o.o.
+
+This file is part of the PrivMX Platform (https://privmx.dev).
+This software is Licensed under the PrivMX Free License.
+
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef _PRIVMXLIB_ENDPOINT_STORE_FILE_HPP_
+#define _PRIVMXLIB_ENDPOINT_STORE_FILE_HPP_
+
+#include "privmx/endpoint/core/Buffer.hpp"
+#include "privmx/endpoint/store/interfaces/IChunkEncryptor.hpp"
+#include "privmx/endpoint/store/interfaces/IHashList.hpp"
+
+namespace privmx {
+namespace endpoint {
+namespace store {
+
+class ServerSliceProvider
+{
+public:
+    ServerSliceProvider(std::shared_ptr<ServerApi> server, const std::string& fileId) : _server(std::move(server)), _fileId(fileId) {}
+    std::string get(int64_t start, int64_t end, int64_t version) {
+        auto range = utils::TypedObjectFactory::createNewObject<server::BufferReadRangeSlice>();
+        range.from(start);
+        range.to(end);
+
+        auto fileDataModel = utils::TypedObjectFactory::createNewObject<server::StoreFileReadModel>();
+        fileDataModel.fileId(_fileId);
+        fileDataModel.version(version);
+        fileDataModel.range(range);
+        fileDataModel.thumb(false);
+        auto fileData = _server->storeFileRead(fileDataModel);
+        return fileData.data();
+    }
+
+private:
+    std::shared_ptr<ServerApi> _server;
+    std::string _fileId;
+};
+
+} // store
+} // endpoint
+} // privmx
+
+#endif // _PRIVMXLIB_ENDPOINT_STORE_FILE_HPP_
