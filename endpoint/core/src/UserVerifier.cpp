0e4476ecf8260fa08a1ec367ba857605edd6fda6
Jerzy Czarkowski <jczarkowski@simplito.com>
2025-06-05 15:01:36 +0200

    fix: spelling error

--- endpoint/core/src/UserVerifier.cpp
+++ endpoint/core/src/UserVerifier.cpp
@@ -19,7 +19,7 @@ UserVerifier::UserVerifier(std::shared_ptr<UserVerifierInterface> userVerifier) 
 std::vector<bool> UserVerifier::verify(const std::vector<VerificationRequest>& request) {
     auto result = _userVerifier->verify(request);
     if(result.size() != request.size()) {
-        throw MalformedVerifierResponseException("VerificationResult size is " + std::to_string(result.size()) + " with is different from VerificationRequest size" + std::to_string(request.size()));
+        throw MalformedVerifierResponseException("VerificationResult size is " + std::to_string(result.size()) + " which is different from VerificationRequest size" + std::to_string(request.size()));
     }
     return result;
 };
