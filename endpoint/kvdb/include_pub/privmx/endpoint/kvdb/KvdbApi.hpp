19fdd528e9e0d6ee38eef0643ebc37e80133d2c3
Kamil Zuranski <zuranski83@gmail.com>
2025-06-17 16:43:30 +0200

    doc: change all the kvdb word occurances

--- endpoint/kvdb/include_pub/privmx/endpoint/kvdb/KvdbApi.hpp
+++ endpoint/kvdb/include_pub/privmx/endpoint/kvdb/KvdbApi.hpp
@@ -9,7 +9,7 @@
 
 #include "privmx/endpoint/core/Connection.hpp"
 #include "privmx/endpoint/core/Types.hpp"
-#include "privmx/endpoint/kvdb/Types.hpp"
+#include "privmx/endpoint/KVDB/Types.hpp"
 
 namespace privmx {
 namespace endpoint {
@@ -37,15 +37,15 @@ public:
     KvdbApi() = default;
 
     /**
-     * Creates a new Kvdb in given Context.
+     * Creates a new KVDB in given Context.
      *
-     * @param contextId ID of the Context to create the Kvdb in
-     * @param users array of UserWithPubKey structs which indicates who will have access to the created Kvdb
-     * @param managers array of UserWithPubKey structs which indicates who will have access (and management rights) to the created Kvdb
+     * @param contextId ID of the Context to create the KVDB in
+     * @param users array of UserWithPubKey structs which indicates who will have access to the created KVDB
+     * @param managers array of UserWithPubKey structs which indicates who will have access (and management rights) to the created KVDB
      * @param publicMeta public (unencrypted) metadata
      * @param privateMeta private (encrypted) metadata
-     * @param policies Kvdb's policies
-     * @return ID of the created Kvdb
+     * @param policies KVDB's policies
+     * @return ID of the created KVDB
      */
     std::string createKvdb(const std::string& contextId, 
         const std::vector<core::UserWithPubKey>& users,
@@ -56,17 +56,17 @@ public:
     );
 
     /**
-     * Updates an existing Kvdb.
+     * Updates an existing KVDB.
      *
-     * @param kvdbId ID of the Kvdb to update
-     * @param users array of UserWithPubKey structs which indicates who will have access to the created Kvdb
-     * @param managers array of UserWithPubKey structs which indicates who will have access (and management rights) to the created Kvdb
+     * @param kvdbId ID of the KVDB to update
+     * @param users array of UserWithPubKey structs which indicates who will have access to the created KVDB
+     * @param managers array of UserWithPubKey structs which indicates who will have access (and management rights) to the created KVDB
      * @param publicMeta public (unencrypted) metadata
      * @param privateMeta private (encrypted) metadata
-     * @param version current version of the updated Kvdb
+     * @param version current version of the updated KVDB
      * @param force force update (without checking version)
-     * @param forceGenerateNewKey force to regenerate a key for the Kvdb
-     * @param policies Kvdb's policies
+     * @param forceGenerateNewKey force to regenerate a key for the KVDB
+     * @param policies KVDB's policies
      */    
     void updateKvdb(const std::string& kvdbId, 
         const std::vector<core::UserWithPubKey>& users,
@@ -80,19 +80,19 @@ public:
     );
 
     /**
-     * Deletes a Kvdb by given Kvdb ID.
+     * Deletes a KVDB by given KVDB ID.
      *
-     * @param kvdbId ID of the Kvdb to delete
+     * @param kvdbId ID of the KVDB to delete
      */
     void deleteKvdb(const std::string& kvdbId);
 
     /**
-     * Gets a Kvdb by given Kvdb ID.
+     * Gets a KVDB by given KVDB ID.
      *
-     * @param kvdbId ID of Kvdb to get
-     * @return struct containing info about the Kvdb
+     * @param kvdbId ID of KVDB to get
+     * @return struct containing info about the KVDB
      */
-    Kvdb getKvdb(const std::string& kvdbId);
+    KVDB getKvdb(const std::string& kvdbId);
 
     /**
      * Gets a list of Kvdbs in given Context.
@@ -101,82 +101,82 @@ public:
      * @param pagingQuery with list query parameters
      * @return struct containing a list of Kvdbs
      */    
-    core::PagingList<Kvdb> listKvdbs(const std::string& contextId, const core::PagingQuery& pagingQuery);
+    core::PagingList<KVDB> listKvdbs(const std::string& contextId, const core::PagingQuery& pagingQuery);
 
     /**
-     * Gets a kvdb entry by given kvdb entry key and kvdb ID.
+     * Gets a KVDB entry by given KVDB entry key and KVDB ID.
      *
-     * @param kvdbId kvdb ID of the kvdb entry to get
-     * @param key key of the kvdb entry to get
-     * @return struct containing the kvdb entry
+     * @param kvdbId KVDB ID of the KVDB entry to get
+     * @param key key of the KVDB entry to get
+     * @return struct containing the KVDB entry
      */    
     KvdbEntry getEntry(const std::string& kvdbId, const std::string& key);
 
     /**
-     * Gets a list of kvdb entries keys from a Kvdb.
+     * Gets a list of KVDB entries keys from a KVDB.
      *
-     * @param kvdbId ID of the Kvdb to list kvdb entries from
+     * @param kvdbId ID of the KVDB to list KVDB entries from
      * @param pagingQuery with list query parameters
-     * @return struct containing a list of kvdb entries
+     * @return struct containing a list of KVDB entries
      */    
     core::PagingList<std::string> listEntriesKeys(const std::string& kvdbId, const core::PagingQuery& pagingQuery);
 
     /**
-     * Gets a list of kvdb entries from a Kvdb.
+     * Gets a list of KVDB entries from a KVDB.
      *
-     * @param kvdbId ID of the Kvdb to list kvdb entries from
+     * @param kvdbId ID of the KVDB to list KVDB entries from
      * @param pagingQuery  with list query parameters
-     * @return struct containing a list of kvdb entries
+     * @return struct containing a list of KVDB entries
      */    
     core::PagingList<KvdbEntry> listEntries(const std::string& kvdbId, const core::PagingQuery& pagingQuery);
 
     /**
-     * Sets a kvdb entry in the given Kvdb.
-     * @param kvdbId ID of the Kvdb to set the entry to
-     * @param key kvdb entry key
-     * @param publicMeta public kvdb entry metadata
-     * @param privateMeta private kvdb entry metadata
-     * @param data content of the kvdb entry
-     * @return ID of the new kvdb entry
+     * Sets a KVDB entry in the given KVDB.
+     * @param kvdbId ID of the KVDB to set the entry to
+     * @param key KVDB entry key
+     * @param publicMeta public KVDB entry metadata
+     * @param privateMeta private KVDB entry metadata
+     * @param data content of the KVDB entry
+     * @return ID of the new KVDB entry
      */    
     void setEntry(const std::string& kvdbId, const std::string& key, const core::Buffer& publicMeta, const core::Buffer& privateMeta, const core::Buffer& data, int64_t version);
     
     /**
-     * Deletes a kvdb entry by given kvdb entry ID.
+     * Deletes a KVDB entry by given KVDB entry ID.
      *
-     * @param kvdbId kvdb ID of the kvdb entry to delete
-     * @param key key of the kvdb entry to delete
+     * @param kvdbId KVDB ID of the KVDB entry to delete
+     * @param key key of the KVDB entry to delete
      */    
     void deleteEntry(const std::string& kvdbId, const std::string& key);
 
     /**
-     * Deletes a kvdb entries by given kvdb ID and the list of entries keys.
+     * Deletes KVDB entries by given KVDB IDs and the list of entry keys.
      *
-     * @param kvdbId ID of the kvdb database to delete from
-     * @param keys vector of the keys of the kvdb entries to delete
+     * @param kvdbId ID of the KVDB database to delete from
+     * @param keys vector of the keys of the KVDB entries to delete
      * @return map with the statuses of deletion for every key
      */    
     std::map<std::string, bool> deleteEntries(const std::string& kvdbId, const std::vector<std::string>& keys);
 
     /**
-     * Subscribes for the Kvdb module main events.
+     * Subscribes for the KVDB module main events.
      */
     void subscribeForKvdbEvents();
     
     /**
-     * Unsubscribes from the Kvdb module main events.
+     * Unsubscribes from the KVDB module main events.
      */
     void unsubscribeFromKvdbEvents();
 
     /**
-     * Subscribes for events in given Kvdb.
-     * @param ID of the Kvdb to subscribe
+     * Subscribes for events in given KVDB.
+     * @param ID of the KVDB to subscribe
      */    
     void subscribeForEntryEvents(std::string kvdbId);
 
     /**
-     * Unsubscribes from events in given Kvdb.
-     * @param {string} kvdbId ID of the Kvdb to unsubscribe
+     * Unsubscribes from events in given KVDB.
+     * @param {string} kvdbId ID of the KVDB to unsubscribe
      */    
     void unsubscribeFromEntryEvents(std::string kvdbId);
 
