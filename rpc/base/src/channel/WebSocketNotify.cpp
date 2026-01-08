34166f40e3bd17a639bc6c0604101b95536e743c
Kamil Zuranski <zuranski83@gmail.com>
2026-01-08 13:49:38 +0100

    refactor: code cleanup

--- rpc/base/src/channel/WebSocketNotify.cpp
+++ rpc/base/src/channel/WebSocketNotify.cpp
@@ -9,197 +9,7 @@
 // limitations under the License.
 // */
 //
-// #include <Poco/ByteOrder.h>
-//
-// #include <privmx/rpc/RpcException.hpp>
-// #include <privmx/rpc/channel/WebSocketNotify.hpp>
-//
-// #include "privmx/utils/Logger.hpp"
-//
-// using namespace privmx;
-// using namespace privmx::rpc;
-// using namespace privmx::utils;
-// using namespace std;
-// using namespace Poco;
-// using namespace Poco::JSON;
-//
-// WebSocketNotify::~WebSocketNotify() {
-//     cancelNotifier();
-// }
-//
-//
-// void WebSocketNotify::add(Int32 wschannelid, function<void(const string&)> callback, function<void(void)> on_websocket_close) {
-//     Lock lock(_mutex);
-//     _ws_channel_funcs[wschannelid] = make_pair(callback, on_websocket_close);
-// }
-//
-// void WebSocketNotify::remove(Int32 wschannelid) {
-//     bool shouldCloseAll = false;
-//     {
-//         LOG_DEBUG("webSocketNotify 1")
-//         Lock lock(_mutex);
-//         LOG_DEBUG("webSocketNotify 2")
-//         auto it = _ws_channel_funcs.find(wschannelid);
-//         if (it == _ws_channel_funcs.end()) {
-//             LOG_DEBUG("webSocketNotify: InvalidWsChannelIdException")
-//             throw InvalidWsChannelIdException();
-//         }
-//         LOG_DEBUG("webSocketNotify 3")
-//         _ws_channel_funcs.erase(it);
-//         shouldCloseAll = _ws_channel_funcs.empty();
-//     }
-//     if (shouldCloseAll && on_close_all_channels) {
-//         LOG_DEBUG("webSocketNotify 4")
-//         on_close_all_channels();
-//     }
-//     if (_notifier_active) {
-//         LOG_DEBUG("webSocketNotify 5")
-//         if(!_notifier_cancellation_token.isNull()) {
-//             LOG_DEBUG("webSocketNotify 6")
-//             _notifier_cancellation_token->cancel();
-//         }
-//         LOG_DEBUG("webSocketNotify 7")
-//         _notify_cv.notify_one();
-//         // if (_consumer_thread.joinable()) {
-//         LOG_DEBUG("webSocketNotify 8")
-//             // _consumer_thread.join();
-//             cancelNotifier();
-//
-//         // }
-//         _notifier_active = false;
-//         LOG_DEBUG("webSocketNotify 9")
-//     }
-// }
-//
-// void WebSocketNotify::notify(const string& data) {
-//     Int32 wschannelid = ByteOrder::fromBigEndian(*((Int32*)data.data()));
-//     function<void(const string&)> callback;
-//     {
-//         Lock lock(_mutex);
-//         auto it = _ws_channel_funcs.find(wschannelid);
-//         if(it == _ws_channel_funcs.end()){
-//             throw InvalidWsChannelIdException();
-//         }
-//         callback = (*it).second.first;
-//     }
-//     if (callback) {
-//         callback(data.substr(4));
-//     }
-// }
-//
-// void WebSocketNotify::queueForNotify(const string data) {
-//     bool expected = false;
-//     if (_notifier_active.compare_exchange_strong(expected, true)) {
-//         _notifier_cancellation_token = privmx::utils::CancellationToken::create();
-//         _consumer_thread = std::thread([&](privmx::utils::CancellationToken::Ptr token){
-//             while(!token->isCancelled()) {
-//                 LOG_DEBUG("queueForNotify: notifier()")
-//                 notifier();
-//                 LOG_DEBUG("queueForNotify: after notifier()")
-//             }
-//             LOG_DEBUG("queueForNotify: after while")
-//         }, _notifier_cancellation_token);
-//     }
-//
-//     Int32 wschannelid = ByteOrder::fromBigEndian(*((Int32*)data.data()));
-//     function<void(const string&)> callback;
-//     {
-//         Lock lock(_mutex);
-//         auto it = _ws_channel_funcs.find(wschannelid);
-//         if(it == _ws_channel_funcs.end()){
-//             throw InvalidWsChannelIdException();
-//         }
-//         callback = (*it).second.first;
-//     }
-//     if (callback) {
-//         CallbackWithData pair = {
-//             .data = data,
-//             .callback = callback
-//         };
-//         _notificationsQueue.push(pair);
-//
-//         lock_guard<std::mutex>lock(_notifyMutex);
-//         _data_to_notify = true;
-//         _notify_cv.notify_one();
-//     }
-// }
-//
-// void WebSocketNotify::onWebSocketClose() {
-//     cancelNotifier();
-//     unordered_map<Int32, WsChannelFuncs> ws_channel_funcs_local;
-//     {
-//         Lock lock(_mutex);
-//         ws_channel_funcs_local = _ws_channel_funcs;
-//     }
-//     for (auto &funcs: ws_channel_funcs_local) {
-//         funcs.second.second();
-//     }
-// }
-//
-// void WebSocketNotify::notifier() {
-//     // unique_lock<std::mutex>lock(_notifyMutex);
-//     // _notify_cv.wait(lock, [&] {
-//     //     return _data_to_notify.load();
-//     // });
-//     // while(_notificationsQueue.size() > 0) {
-//     //     auto ret {_notificationsQueue.pop()};
-//     //     ret.callback(ret.data.substr(4));
-//     // };
-//     //
-//     // _data_to_notify.store(false);
-//     LOG_DEBUG("notifier 1");
-//     unique_lock<std::mutex>lock(_notifyMutex);
-//     _notify_cv.wait(lock, [&] {
-//         return _data_to_notify.load();
-//     });
-//     LOG_DEBUG("notifier 2");
-//
-//     if(_notifier_cancellation_token->isCancelled()) {
-//         LOG_DEBUG("notifier 3");
-//         _data_to_notify.store(false);
-//         return;
-//     }
-//     LOG_DEBUG("notifier 4");
-//
-//     while(_notificationsQueue.size() > 0) {
-//         auto ret {_notificationsQueue.pop()};
-//         ret.callback(ret.data.substr(4));
-//     };
-//     LOG_DEBUG("notifier 5");
-//
-//
-//     _data_to_notify.store(false);
-//     LOG_DEBUG("notifier 6");
-//
-// }
-//
-// void WebSocketNotify::cancelNotifier() {
-//     LOG_DEBUG("cancelNotifier 1");
-//     if(!_notifier_cancellation_token.isNull()) {
-//         _notifier_cancellation_token->cancel();
-//     }
-//     _data_to_notify.store(true);
-//     LOG_DEBUG("cancelNotifier 4");
-//     _notify_cv.notify_one();
-//     LOG_DEBUG("cancelNotifier 5");
-//
-//     if (_consumer_thread.joinable() &&
-//         _consumer_thread.get_id() != std::this_thread::get_id()) {
-//         LOG_DEBUG("cancelNotifier 6");
-//
-//         _consumer_thread.join();
-//     }
-//     LOG_DEBUG("cancelNotifier 7");
-//
-// }
-/*
-PrivMX Endpoint.
-Copyright © 2024 Simplito sp. z o.o.
-This file is part of the PrivMX Platform (https://privmx.dev).
-This software is Licensed under the PrivMX Free License.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+
 #include <Poco/ByteOrder.h>
 #include <privmx/rpc/RpcException.hpp>
 #include <privmx/rpc/channel/WebSocketNotify.hpp>
@@ -431,7 +241,6 @@ void WebSocketNotify::cancelNotifier() {
     LOG_DEBUG("cancelNotifier 5: notifying cv");
     _notify_cv.notify_one();
 
-    // #if !defined(__EMSCRIPTEN__)
         if (_consumer_thread.joinable() && _consumer_thread.get_id() != std::this_thread::get_id()) {
             LOG_DEBUG("cancelNotifier 6: joinable, joining");
             _consumer_thread.join();
@@ -439,10 +248,7 @@ void WebSocketNotify::cancelNotifier() {
         } else {
                 LOG_DEBUG("cancelNotifier 6: not joinable or same thread, skipping join");
         }
-    // #else
 
-        // LOG_DEBUG("cancelNotifier 6: WASM build – skipping join()");
-    // #endif
 
     LOG_DEBUG("cancelNotifier: end");
 }
