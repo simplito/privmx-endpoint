e87685aca6c009e6750d62c8eda2885cb0ede10d
Patryk Kisielewski <pkisielewski@simplito.com>
2026-02-02 13:41:54 +0100

    build(android): add optional build for helper library

---
+++ endpoint/android/include_pub/privmx/endpoint/android/cinterface/android.h
@@ -0,0 +1,20 @@
+#ifndef _PRIVMX_ENDPOINT_ANDROID_INTERFACE_API_
+#define _PRIVMX_ENDPOINT_ANDROID_INTERFACE_API_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct ThreadApi ThreadApi;
+
+int privmx_endpoint_android_init();
+void* privmx_endpoint_android_dlopen(const char *path, int flags);
+int privmx_endpoint_android_dlclose(void* handle);
+char* privmx_endpoint_android_dlerror(void);
+void* privmx_endpoint_android_dlsym(void* handle, const char* symbol);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _PRIVMX_ENDPOINT_ANDROID_INTERFACE_API_
