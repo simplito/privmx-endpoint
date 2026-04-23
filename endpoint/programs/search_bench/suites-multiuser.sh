e6780506f6af511db55adec5fe5af95bf3b7061c
Kamil Zuranski <zuranski83@gmail.com>
2026-04-23 14:03:17 +0200

    test: add missing files - multiuser suite

--- endpoint/programs/search_bench/suites-multiuser.sh
+++ endpoint/programs/search_bench/suites-multiuser.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 SEARCH_BENCH_SUITES=(
-    "batch_add_1000_to_existing --search-index 69e937ed9ff7e4dccfe784db"
-    "list_and_search --search-index 69e937ed9ff7e4dccfe784db"
-    "batch_add_1000_to_existing --search-index 69e937ed9ff7e4dccfe784db"
-    "list_and_search --search-index 69e937ed9ff7e4dccfe784db"
-    "batch_add_1000_to_existing --search-index 69e937ed9ff7e4dccfe784db"
-    "list_and_search --search-index 69e937ed9ff7e4dccfe784db"
+    "batch_add_1000_to_existing --search-index 69e9c97a9ff7e4dccfe784e0"
+    "list_and_search --search-index 69e9c97a9ff7e4dccfe784e0"
+    "batch_add_1000_to_existing --search-index 69e9c97a9ff7e4dccfe784e0"
+    "list_and_search --search-index 69e9c97a9ff7e4dccfe784e0"
+    "batch_add_1000_to_existing --search-index 69e9c97a9ff7e4dccfe784e0"
+    "list_and_search --search-index 69e9c97a9ff7e4dccfe784e0"
 )
