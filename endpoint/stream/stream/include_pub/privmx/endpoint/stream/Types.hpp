14c7f581cb96be2688a94f76385c694d29551ebb
Jerzy Czarkowski <jczarkowski@simplito.com>
2026-06-16 09:09:33 +0200

    refactor!: removal talking field from StreamApi models (#451)

--- endpoint/stream/stream/include_pub/privmx/endpoint/stream/Types.hpp
+++ endpoint/stream/stream/include_pub/privmx/endpoint/stream/Types.hpp
@@ -95,7 +95,6 @@ struct StreamTrackInfo {
     std::optional<std::string> description; // opis strumienia
     std::optional<bool> moderated;          // czy zmoderowany
     std::optional<bool> simulcast;          // czy używa simulcast
-    std::optional<bool> talking;            // czy aktywność audio
 };
 
 struct StreamInfo {
@@ -104,7 +103,6 @@ struct StreamInfo {
     std::optional<std::string> metadata; // metadane jako tekst JSON
     std::optional<bool> dummy;           // czy to publisher-dummy
     std::vector<StreamTrackInfo> tracks; // lista trackow
-    std::optional<bool> talking;         // deprecated
 };
 
 struct StreamTrackModificationPair {
