0bb3c5cf4e422651db497c0f959877266e6470c4
Jerzy Czarkowski <jczarkowski@simplito.com>
2026-08-10 14:07:17 +0200

    chore: update tests_e2e pipeline (#471)

--- test/test_env/utils/clean_up_volumes.sh
+++ test/test_env/utils/clean_up_volumes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 echo Cleaning all volumes files
-docker run --rm -v "$SCRIPT_PATH/volumes:/volumes" busybox rm -rf /volumes/mongo /volumes/bridge 
+docker run --rm -v "$SCRIPT_PATH/volumes:/volumes" busybox sh -c "rm -rf /volumes/mongo /volumes/bridge && mkdir -p /volumes/bridge/storage && chmod -R 777 /volumes/bridge" 
